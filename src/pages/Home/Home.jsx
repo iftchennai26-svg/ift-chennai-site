28cb3d007d6d1ac304762e72bf64a47c6439ef96
IFT Chennai <iftchennai26@gmail.com>
2026-04-25 16:41:32 +0530

    fix import path

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import HeroSection from '@/Components/HeroSection/HeroSection';
-import CategoriesSection from '@/Components/CategoriesSection/CategoriesSection';
-import FeaturedBooks from '@/Components/FeaturedBooks/FeaturedBooks';
-import LatestBooks from '@/Components/LatestBooks/LatestBooks';
-import OnSale from '@/Components/OnSale/OnSale';
-import NewArrivals from '@/Components/NewArrivals/NewArrivals';
-import BestsellingBooks from '@/Components/BestsellingBooks/BestsellingBooks';
-import WhyChooseUs from '@/Components/WhyChooseUs/WhyChooseUs';
+import HeroSection from '../../Components/HeroSection/HeroSection';
+import CategoriesSection from '../../Components/CategoriesSection/CategoriesSection';
+import FeaturedBooks from '../../Components/FeaturedBooks/FeaturedBooks';
+import LatestBooks from '../../Components/LatestBooks/LatestBooks';
+import OnSale from '../../Components/OnSale/OnSale';
+import NewArrivals from '../../Components/NewArrivals/NewArrivals';
+import BestsellingBooks from '../../Components/BestsellingBooks/BestsellingBooks';
+import WhyChooseUs from '../../Components/WhyChooseUs/WhyChooseUs';
 import './Home.css';
 
 const Home = () => {
